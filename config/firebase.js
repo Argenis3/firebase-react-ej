e0ce8441c3565462695727f9f5096a662baf9bd6
ArgenisAragon <argenisss70@gmail.com>
2025-11-05 16:12:30 -0600

    firebase conectada

--- config/firebase.js
+++ config/firebase.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDONe70CfAgFs7HVEBHy-7tW5RgwCfPfd8",
-  authDomain: "fir-app-c45be.firebaseapp.com",
-  projectId: "fir-app-c45be",
-  storageBucket: "fir-app-c45be.firebasestorage.app",
-  messagingSenderId: "709239679287",
-  appId: "1:709239679287:web:890f5764392fb56bba12b2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export default app;
